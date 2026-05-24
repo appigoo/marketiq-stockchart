edc78fcc6e53b8c0aa0195392d99ffb0205162f0
appigoo <appigoodoc@gmail.com>
2026-05-24 17:29:41 +0100

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = { title: 'MarketIQ' }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="zh">
+      <body style={{margin:0}}>{children}</body>
+    </html>
+  )
+}
